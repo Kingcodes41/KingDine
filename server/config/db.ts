cd5fe651b1c73288a370540a033283a32a0b45fa
Abimaje Gideon Chubiojo <abimajegideonchubiojo@gmail.com>
2026-08-09 23:58:30 +0100

    fix:read online database string from environment

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     // FORCE IPv4 address to bypass local DNS lookup bugs
-    const mongoURI = "mongodb://127.0.0.1:27017/king-dine";
+    const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/king-dine";
     
     await mongoose.connect(mongoURI, {
       serverSelectionTimeoutMS: 3000, // Drop connection attempt after 3 seconds instead of 10
