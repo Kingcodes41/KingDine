1a31a9b2a7922c71cdd61955595dd184cf2ac2b8
Abimaje Gideon Chubiojo <abimajegideonchubiojo@gmail.com>
2026-08-11 13:32:10 +0100

    Port changed

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.use("/api/auth",authRouter);
 app.use("/api/restaurant",restaurantRouter);
